2cfcd4f94bafc80d1fc8d044f5c918f094e4795e
Idriss Douiri <drisspennywise@gmail.com>
2025-06-09 12:08:37 +0000

    Add github link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { CiExport } from "react-icons/ci";
 import ExportModal from "./ExportModal";
 import { useRef } from "react";
+import { BsGithub } from "react-icons/bs";
 
 interface Props {
   pathConfig: {
@@ -22,12 +23,22 @@ const Header = ({ pathConfig }: Props) => {
         <a href="#" className="text-coffee">
           <img src="/logo.svg" width={40} height={40} />
         </a>
-        <button
-          onClick={() => modalRef.current?.showModal()}
-          className="flex items-center gap-2 bg-frappe text-gray-50 hover:brightness-110 transition-all py-2 px-4 rounded-2xl"
-        >
-          <CiExport size={25} /> Export
-        </button>
+        <div className="flex items-center gap-4">
+          <a
+            href="https://github.com/AstroDriss/inverted-corners-generator"
+            title="this project on github"
+            aria-label="Visit this project at github"
+            target="_blank"
+          >
+            <BsGithub size={25} />
+          </a>
+          <button
+            onClick={() => modalRef.current?.showModal()}
+            className="flex items-center gap-2 bg-frappe text-gray-50 hover:brightness-110 transition-all py-2 px-4 rounded-2xl"
+          >
+            <CiExport size={25} /> Export
+          </button>
+        </div>
       </header>
 
       <ExportModal ref={modalRef} pathConfig={pathConfig} />
